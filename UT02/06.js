d592f154f27c850c4a76a5514d379eb66fdfb89c
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-09-28 12:45:46 +0200

    07

--- UT02/06.js
+++ UT02/06.js
@@ -1,5 +1,5 @@
 /*
-Diseñar un algoritmo que tomando las coordenadas cartesianas de un punto en el plano y calcule e imprima el cuadrante al que pertenece dicho punto.
+6. Diseñar un algoritmo que tomando las coordenadas cartesianas de un punto en el plano y calcule e imprima el cuadrante al que pertenece dicho punto.
 También debe detectar cuando está en el origen o sobre un eje.
 */
 
---
+++ UT02/07.js
@@ -0,0 +1,9 @@
+/*
+7. Elabora un programa que dado un precio y una cantidad pagada, obtengamos el cambio con el mínimo número de elementos (monedas o billetes).
+Para devolver el cambio disponemos de una cantidad ilimitada de monedas y de billetes.
+*/
+
+var mensaje;
+var precio = prompt("Precio");
+var cantidadPagada = prompt("Cantidad pagada");
+
