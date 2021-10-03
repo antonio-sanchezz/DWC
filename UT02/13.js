8ab023c1610ec93475913c2b734446bc8500a08d
Antonio Sanchez Espinosa <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-10-03 16:26:59 +0200

    13 completado

--- UT02/13.js
+++ UT02/13.js
@@ -1,15 +1,22 @@
 var dia;
 var mes;
 var year;
+var tarot_in;
+var tarot_fin = 0;
 
 dia = parseInt(prompt("Indice su dia de nacimiento:"));
 mes = parseInt(prompt("Indice su mes de nacimiento:"));
 year = parseInt(prompt("Indice su año de nacimiento:"));
 
+
 if (dia <= 30 && dia > 0) {
     if (mes <= 12 && mes > 0) {
         if (Number.isInteger(year)) {
-
+            tarot_in = (dia + mes + year).toString();
+            for (i = 0; tarot_in.length > i; i++) {
+                tarot_fin = tarot_fin + parseInt(tarot_in.substr(i,1));
+            }
+            document.write("Tarot: " + tarot_fin);
         } else {
             document.write("Formato de año incorrecto.");
         }
