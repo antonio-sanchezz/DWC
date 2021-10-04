a5d2a5ebc6d8034e19c06e58a25e341ff10f0682
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-10-04 13:42:19 +0200

    14 completado

--- UT02/14.js
+++ UT02/14.js
@@ -2,6 +2,10 @@ var sexo;
 var sueldo;
 var validadorSexo = true;
 var validadorSueldo = true;
+var mediaHombre = 0;
+var mediaMujer = 0;
+var contadorM = 0;
+var contadorH = 0;
 
 do {
     do {
@@ -19,6 +23,13 @@ do {
                 if(Number.isInteger(sueldo) && sueldo >= 1000 && sueldo <= 2000)
                 {
                     validadorSueldo = false;
+                    if (sexo == "H") {
+                        mediaHombre += sueldo;
+                        contadorH++;
+                    } else {
+                        mediaMujer += sueldo;
+                        contadorM++;
+                    }
                     document.write("Sexo: " + sexo + "<br>");
                     document.write("Sueldo: " + sueldo + "<br>");
                 } else
@@ -32,3 +43,8 @@ do {
         }
     } while(validadorSexo);
 } while(sexo != "*");
+
+document.write("Media hombres<br>");
+document.write("Sueldo: " + mediaHombre/contadorH + "<br>");
+document.write("Media mujeres<br>");
+document.write("Sueldo: " + mediaMujer/contadorM + "<br>");
