b376297154e9cd5d36c11c5fdf5301a3187da803
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-10-04 13:03:00 +0200

    Nueva forma

--- UT02/13.js
+++ UT02/13.js
@@ -8,6 +8,8 @@ dia = parseInt(prompt("Indice su dia de nacimiento:"));
 mes = parseInt(prompt("Indice su mes de nacimiento:"));
 year = parseInt(prompt("Indice su año de nacimiento:"));
 
+var sumFecha = dia + mes + year;
+var sum = 0;
 
 if (dia <= 30 && dia > 0) {
     if (mes <= 12 && mes > 0) {
@@ -16,7 +18,7 @@ if (dia <= 30 && dia > 0) {
             for (i = 0; tarot_in.length > i; i++) {
                 tarot_fin = tarot_fin + parseInt(tarot_in.substr(i,1));
             }
-            document.write("Tarot: " + tarot_fin);
+            document.write("Tarot: " + tarot_fin + "<br>");
         } else {
             document.write("Formato de año incorrecto.");
         }
@@ -26,3 +28,14 @@ if (dia <= 30 && dia > 0) {
 } else {
     document.write("Formato de día incorrecto.");
 }
+
+do {
+    if(sumFecha == 0) {
+        sumFecha = sum;
+        sum = 0;
+    }
+    sum += sumFecha % 10;
+    sumFecha = parseInt(sumFecha / 10);
+} while (sumFecha >= 1 || sum > 9);
+
+document.write("Tarot: " + sum);
