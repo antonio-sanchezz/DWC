06cd51e5653a925c3445de159d618f0a5f1175df
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-10-05 13:10:43 +0200

    9

---
+++ UT03/09.js
@@ -0,0 +1,11 @@
+var contadorPalabras = 0;
+var contadorA = 0;
+
+var texto = "El perro de san Roque no tiene rabo";
+
+if (texto.charAt(0) == "A") {
+    contadorA++;
+}
+
+document.write("Palabras: " + contadorPalabras + "<br>");
+document.write("Palabras que comienza por A: " + contadorC + "<br>");
