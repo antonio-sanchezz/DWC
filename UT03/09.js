ce7038b012a47803b498bb13d0e7b74a06f8c6ad
Antonio Sanchez Espinosa <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-10-06 19:40:13 +0200

    9-10

--- UT03/09.js
+++ UT03/09.js
@@ -1,11 +1,17 @@
 var contadorPalabras = 0;
 var contadorA = 0;
 
-var texto = "El perro de san Roque no tiene rabo";
+var texto = "El perro de san Roque no tiene rabo ahora si tiene";
 
-if (texto.charAt(0) == "A") {
-    contadorA++;
+var palabra = texto.split (" ");
+//Contamos todos los trozos de cadenas que existen
+contadorPalabras = palabra.length;
+
+for(var i= 0; i < contadorPalabras;i++) {
+    if (palabra[i].charAt(0) == "A" || palabra[i].charAt(0) == "a") {
+        contadorA++;
+    }
 }
 
 document.write("Palabras: " + contadorPalabras + "<br>");
-document.write("Palabras que comienza por A: " + contadorC + "<br>");
+document.write("Palabras que comienza por A: " + contadorA + "<br>");
