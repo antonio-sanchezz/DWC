224806a94d32f700ec012a37550b9954f7af7903
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-10-19 12:28:21 +0200

    23,25

--- UT03/23.js
+++ UT03/23.js
@@ -1,18 +1,32 @@
-var dia1 = parseInt(prompt("Indice su dia de fecha 1:"));
-var mes1 = parseInt(prompt("Indice su mes de fecha 1:"));
-var year1 = parseInt(prompt("Indice su año de fecha 1:"));
+do
+{
+var dia1 = parseInt(prompt("Indice su dia de fecha:"));
+var mes1 = parseInt(prompt("Indice su mes de fecha:"));
+var year1 = parseInt(prompt("Indice su año de fecha:"));
+var terminar = prompt("¿Otra fecha? (S/N)");
 
-var dia2 = parseInt(prompt("Indice su dia de fecha 2:"));
-var mes2 = parseInt(prompt("Indice su mes de fecha 2:"));
-var year2 = parseInt(prompt("Indice su año de fecha 2:"));
+var fecha = new Date(year1, mes1-1, dia1).getTime();
+var fechaMay;
+var fechaMen;
 
-var fecha1 = new Date(year1, mes1-1, dia1).getTime();
-var fecha2 = new Date(year2, mes2-1, dia2).getTime();
-
-if (fecha1 > fecha2) {
-    document.write("La fecha 2 introducida es menor a la fecha 1<br>");
-    document.write("La diferencia entre las fechas en segundos es de: " + (fecha1 - fecha2)/1000);
-} else {
-    document.write("La fecha 2 introducida es mayor a la fecha 1<br>");
-    document.write("La diferencia entre las fechas en segundos es de: " + (fecha2 - fecha1)/1000);
+if (!fechaMay && !fechaMen)
+{
+    fechaMay = fecha;
+    fechaMen = fecha;
 }
+else
+{
+    if (fecha > fechaMay)
+    {
+        fechaMay = fecha;
+    }
+    else if (fecha < fechaMen)
+    {
+        fechaMen = fecha;
+    }
+}
+
+} while (terminar == "S");
+
+document.write("Fecha menor: " + new Date(fechaMen).toLocaleDateString() + "<br>");
+document.write("Fecha mayor: " + new Date(fechaMay).toLocaleDateString());
---
+++ UT03/25.js
@@ -0,0 +1,47 @@
+var dia = parseInt(prompt("Indice su dia de nacimiento:"));
+var mes = parseInt(prompt("Indice su mes de nacimiento:"));
+var year = parseInt(prompt("Indice su año de nacimiento:"));
+
+var hoy = new Date();
+var hoyM = hoy.getMonth();
+var hoyD = hoy.getDay();
+var hoyY = hoy.getFullYear();
+
+var nacimiento = new Date(year, mes-1, dia).getTime();
+
+var contador = 0;
+var cumple;
+
+while(contador < 50)
+{
+
+    proximoCumple = new Date(hoyY+contador, mes-1, dia);
+
+    switch(proximoCumple.getDay())
+    {
+        case 0:
+            cumple = "Lunes";
+            break;
+        case 1:
+            cumple = "Martes"
+            break;
+        case 2:
+            cumple = "Miercoles";
+            break;
+        case 3:
+            cumple = "Jueves";
+            break;
+        case 4:
+            cumple = "Viernes";
+            break;
+        case 5:
+            cumple = "Sabado";
+            break;
+        case 6:
+            cumple = "Domingo";
+            break;
+    }
+
+    document.write(cumple + "<br>");
+    contador++;
+}
