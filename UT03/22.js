d62b614601cf51210f021d6a6554b44c443571d7
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-10-20 08:58:11 +0200

    Tiempo que el usuario utiliza el programa

--- UT03/22.js
+++ UT03/22.js
@@ -1,4 +1,9 @@
+var nowSeconds = new Date().getSeconds();
 var segundos = parseInt(prompt("Segundos:"));
+
+// Tiempo que el usuario utiliza en el programa.
+var lastSeconds = new Date().getSeconds();
+var totalSeconds = lastSeconds - nowSeconds;
 
 var horas = Math.floor(new Date(segundos*1000).getHours());
 var minutos = Math.floor(new Date((segundos%3600)*1000).getMinutes());
@@ -7,3 +12,5 @@ var segundos = Math.floor(new Date(((segundos%3600%60))*1000).getSeconds());
 document.write("Horas: " + horas + "<br>");
 document.write("Minutos: " + minutos + "<br>");
 document.write("Segundos: " + segundos);
+
+alert("Has tardado " + totalSeconds + " segundos en ejecutar el programa.");
