01a5e76a1cf88bb12623b14cc6887cc3a19a9110
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-11-24 09:31:56 +0100

    estructura

--- EXAMEN/UT4.P1.SanchezEspinosaAntonio/ej2/2.js
+++ EXAMEN/UT4.P1.SanchezEspinosaAntonio/ej2/2.js
@@ -1,26 +1,22 @@
-do {
-
+var ventana;
 var usuario = prompt("Nombre de usuario:", "antsa12");
 
 if(usuario.match(/^([a-zA-z0-9]{7})$/)) {
     // Contar digitos.
     var numDigitos = (usuario.toString().match(/[0-9]/g) || []).length;
 
-    while (numDigitos != 0) {
-        setTimeout('popUp()', 3000);
-        numDigitos--;
+    if (numDigitos > 0) {
+        var intervalo = setInterval(popUp(), 3000);
     }
-
 }
-
-} while (!usuario);
 
 function popUp() {
-    var ventana = window.open("","","location=0,scrollbars=1,height=300,width=400");
-    ventana.moveTo(screen.width,0); 
-    ventana.document.write("Hola usuario. Aquí tienes el producto del año. Atrévete a probarlo!!");
-}
-
-function stopInterval() {
-    clearInterval(interval);
+    if (numDigitos != 0) {
+        numDigitos--;
+        ventana = window.open("","","location=0,scrollbars=1,height=300,width=400");
+        ventana.moveTo(screen.width,screen.height); 
+        ventana.document.write("Hola usuario. Aquí tienes el producto del año. Atrévete a probarlo!!");
+    } else {
+        clearInterval(intervalo);
+    }
 }
