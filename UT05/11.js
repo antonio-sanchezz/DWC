4c3f57275dd87b411b9f0d5daba5677845e5b0b9
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-11-29 14:25:07 +0100

    11

---
+++ UT05/11.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', iniciar);
+
+function iniciar() {
+
+    var nombre = document.formulario.nombre;
+    var direccion = document.formulario.nombre;
+
+    nombre.addEventListener('blur', function() {
+
+    });
+
+    //https://sites.google.com/site/tumentororg/javascript/eventos-focus-y-blur
+}
