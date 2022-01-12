f062224528d459828d6d4c53be163a1ee0560ce6
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2022-01-12 10:07:25 +0100

    7

---
+++ UT06/07.js
@@ -0,0 +1,51 @@
+window.addEventListener('load', iniciar);
+
+function iniciar() {
+
+    generarTabla();
+
+    var enlace = document.createElement('a');
+    enlace.setAttribute("href", "#");
+    let aTexto = document.createTextNode("Ocultar/Mostrar tabla");
+    enlace.appendChild(aTexto);
+
+    document.body.appendChild(enlace);
+
+    enlace.addEventListener("click", showHide);
+
+}
+
+function showHide() {
+    
+    var tabla = document.getElementsByTagName('table')[0];
+    if (tabla != undefined) {
+        tabla.remove();
+    } else {
+        generarTabla();
+    }
+    
+}
+
+function generarTabla() {
+
+    var tabla = document.createElement('table');
+
+    for (let i = 1; i <= 20; i++) {
+
+        let fila = document.createElement('tr');
+        tabla.appendChild(fila);
+
+        for (let j = 1; j <= 20; j++) {
+
+            let columna = document.createElement('td');
+            fila.appendChild(columna);
+
+            let txt = document.createTextNode(j);
+            columna.appendChild(txt);
+        }
+
+    }
+
+    document.body.appendChild(tabla);
+
+}
