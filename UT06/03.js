5f9e18483209ae0d5b96ea7d185e40c816ed08d8
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2022-01-12 09:42:33 +0100

    3

--- UT06/03.js
+++ UT06/03.js
@@ -2,26 +2,40 @@ window.addEventListener('load', iniciar);
 
 function iniciar() {
 
-    var lista = document.getElementsByTagName('ul');
+    var lista = document.getElementsByTagName('li')[0].parentNode;
+    
+    // lista.setAttribute("type", "square"); Esto tambien vale.
+    lista.style.listStyle = 'square';
 
-    // Cambiar diseño.
-    lista[0].style.cssText = 'list-style-type: square;';
-
-    var primero = lista[0].children[0];
-    var ultimo = lista[0].children[lista[0].children.length-1];
-
-    // Ultimo lugar.
+    // Al final.
     var elem1 = document.createElement('li');
-    ultimo.appendChild(elem1);
+    lista.appendChild(elem1);
 
     var txt1 = document.createTextNode('Toyota');
     elem1.appendChild(txt1);
 
     // Primer lugar.
     var elem2 = document.createElement('li');
-    primero.appendChild(elem2);
+    lista.insertBefore(elem2, lista.firstChild);
 
     var txt2 = document.createTextNode('BMW');
     elem2.appendChild(txt2);
 
+    // Buscar un elemento por el contenido y situarlo en medio.
+
+    var pos;
+
+    var lista2 = document.getElementsByTagName('li');
+
+    for (let i= 0; i < lista.childElementCount; i++) {
+        if (lista2[i].textContent == "BMW") {
+            pos = i;
+        }
+    }
+
+    if (pos != undefined) {
+        lista.insertBefore(elem1, lista[pos]);
+    } else {
+        alert("BMW no existe");
+    }
 }
