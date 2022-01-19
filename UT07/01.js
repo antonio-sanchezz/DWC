65a83da24ea2bfe99d72f2fb60c680ac141f4d64
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2022-01-19 09:30:40 +0100

    01

---
+++ UT07/01.js
@@ -0,0 +1,9 @@
+$(function(){
+
+    $("h2").css("color", "red");
+    $(".details").text("$199.99");
+    $("h2").text("Sanlúcar de Barrameda");
+    $("#vacations").css("list-style", "square");
+    $(".america").css("background-color", "yellow");
+
+}); 
