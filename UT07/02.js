95bdfc3bc01b52cdb484c5d14d984c2846abfae5
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2022-01-20 11:01:32 +0100

    sol

--- UT07/02.js
+++ UT07/02.js
@@ -1,6 +1,15 @@
 $(function(){
 
-    $("p").eq(0).next().children().css("color", "red");
-    $("p").eq(0).next().find("li").eq(2).css("color", "green");
-
+    //$("p").eq(0).next().children().css("color", "red");
+    //$("p").eq(0).next().find("li").eq(2).css("color", "green");
+    //$("p").eq(0).next().find("li").eq(2).next().css("color", "purple");
+    //$("p").eq(0).next().find("li").eq(2).nextAll().css("color", "purple");
+    //$("a[href]").css("color", "red");
+    //$("a:not([href])").css("color", "green");
+    //$("a[href *= 'mod']").css("background", "yellow");
+    //$("a[href ^= 'mod']").css("background", "yellow");
+    //$("a[href $= '.html']").css("background", "yellow");
+    //$("p").eq(1).next().find("ul").find("li:last").css("color", "green");
+    //
+    $("p").eq(1).next().children().find("li:nth-child(odd)").css("color", "green");
 });
