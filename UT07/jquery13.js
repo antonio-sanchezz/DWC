7a0c1e907d4510eb561335325c7ad500d2f04733
Antonio Sanchez Espinosa <antonio.sanchez.espinosa.al@iespoligonosur.org>
2022-01-25 21:02:44 +0100

    13

--- UT07/jquery13.js
+++ UT07/jquery13.js
@@ -1,9 +1,9 @@
 $(function(){
 
     $("#bttFade").click(function() {          
-        $("#div1").fadeIn();
-        $("#div2").fadeIn("slow");
-        $("#div3").fadeIn(3000);
+        $("#div1").fadeToggle();
+        $("#div2").fadeToggle("slow");
+        $("#div3").fadeToggle(3000);
     });
 
 });
