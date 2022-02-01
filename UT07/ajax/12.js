15f5e3fd035f02d5f155fd800f28a59f3948db23
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2022-02-01 14:13:50 +0100

    cambios

--- UT07/ajax/12.js
+++ UT07/ajax/12.js
@@ -6,11 +6,32 @@ $(function(){
 
             $("body").append("<div>");
             $("div").attr('id', "todos-los-viajes");
+            $("#todos-los-viajes").append("<ul>");
 
             for (let i = 0; i < datos.length;i++) {
-                $("#todos-los-viajes").append("<ul>").append("<li>").append("<img>", {
-                    'src': datos[i]['src']
+
+                $("<li>").appendTo("ul");
+
+                // Imagen.
+                var imagen = $("<img>", {
+                    src: datos[i]['src']
                 });
+                imagen.appendTo($("li"));
+
+                // Párrafo.
+                var parrafo = $("<p>", {
+                    text: datos[i]['desc']
+                });
+                parrafo.appendTo($("li"));
+
+                // Specs.
+                var lista2 = $("<ul>", {
+                    class: "specs"
+                });
+                lista2.appendTo("li");    
+                for (let j = 0; j < datos[i]['specs'].length;j++) {
+                   $("<li>").appendTo(lista2);
+                }
             }
         }
     });
