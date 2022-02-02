7d45e01de69dbf021d763c176f5529c10dbff12e
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2022-02-02 10:42:37 +0100

    curso

--- UT07/curso/curso.js
+++ UT07/curso/curso.js
@@ -1,29 +1,27 @@
 $(function(){
+    $("#curso").change(function() {
+        var curso = $("#curso").val();
 
-    var curso = $("#curso").val();
+        $.ajax({
+            data: {curso:curso},
+            url: "curso.php",
+            type: "post",
+            success: function(result){
+                var datos = JSON.parse(result);
 
-    $.ajax({
-        data: {curso:curso},
-        url: "curso.php",
-        type: "post",
-        success: function(result){
-            var datos = JSON.parse(result);
+                $("body").append("<div>");
 
-            $("body").append("<div>");
+                $("div:first").addClass("alumnos");
 
-            $("div:first").addClass("alumnos");
+                $("<ul>").appendTo("div:first");
 
-            $("<ul>").appendTo("div:first");
-
-            $("#curso").change(function() {
                 $("ul:first").empty();
                 for (let i = 0; i < datos.length;i++) {
                     var lista = $("<li>");
                     $("ul:first").append(lista);
                     lista.append(datos[i]['nombre'] + " " + datos[i]['apellidos']);
                 }
-            });
-
-        }
-    })
+            }
+        });
+    });
 });
