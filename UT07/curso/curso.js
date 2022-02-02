df5665fb26ddae7503c4e208fe35b23876f3bb9e
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2022-02-02 10:30:06 +0100

    curso

--- UT07/curso/curso.js
+++ UT07/curso/curso.js
@@ -1,8 +1,29 @@
 $(function(){
+
+    var curso = $("#curso").val();
+
     $.ajax({
+        data: {curso:curso},
         url: "curso.php",
+        type: "post",
         success: function(result){
             var datos = JSON.parse(result);
+
+            $("body").append("<div>");
+
+            $("div:first").addClass("alumnos");
+
+            $("<ul>").appendTo("div:first");
+
+            $("#curso").change(function() {
+                $("ul:first").empty();
+                for (let i = 0; i < datos.length;i++) {
+                    var lista = $("<li>");
+                    $("ul:first").append(lista);
+                    lista.append(datos[i]['nombre'] + " " + datos[i]['apellidos']);
+                }
+            });
+
         }
     })
 });
--- UT07/curso/curso.php
+++ UT07/curso/curso.php
@@ -1,7 +1,16 @@
 <?php
 
-$daw1 = [["nombre"=>"Hayri", "apellidos"=>"Ghenadie", "nota"=>5], ["nombre"=>"Theodotos", "apellidos"=>"Arne", "nota"=>6], ["nombre"=>"Amberly", "apellidos"=>"Jenaro", "nota"=>6]];
-$daw2 = [["nombre"=>"Rada", "apellidos"=>"Chares", "nota"=>4], ["nombre"=>"Antonio", "apellidos"=>"Sanchez", "nota"=>10], ["nombre"=>"Pacey", "apellidos"=>"Darinka", "nota"=>6]];
+$data = [];
+$daw1 = [["nombre"=>"Hayri", "apellidos"=>"Ghenadie", "nota"=>5], ["nombre"=>"Theodotos", "apellidos"=>"Arne", "nota"=>6], ["nombre"=>"Amberly", "apellidos"=>"Jenaro", "nota"=>3]];
+$daw2 = [["nombre"=>"Rada", "apellidos"=>"Chares", "nota"=>4], ["nombre"=>"Antonio", "apellidos"=>"Sanchez", "nota"=>10], ["nombre"=>"Pacey", "apellidos"=>"Darinka", "nota"=>9]];
+
+if (isset($_POST["curso"])) {
+    if ($_POST["curso"] == "daw1") {
+        $data = $daw1;
+    } else if ($_POST["curso"] == "daw2") {
+        $data = $daw2;
+    }
+}
 
 echo json_encode($data);
 
